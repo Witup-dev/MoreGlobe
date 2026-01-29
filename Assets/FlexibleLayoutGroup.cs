babed4d3e025487a6ec868f27f48ca6d5e0adea6
carlo-94 <65852549+carlo-94@users.noreply.github.com>
2026-01-29 13:06:23 +0100

    Add files via upload

---
+++ Assets/FlexibleLayoutGroup.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FlexibleLayoutGroup : LayoutGroup
+{
+
+    public enum FitType
+    {
+        Uniform,
+        Width,
+        Height,
+        FixedRows,
+        FixedColumns
+    }
+
+    public FitType fitType;
+    public int rows;
+    public int columns;
+    public Vector2 cellSize;
+    public Vector2 spacing;
+
+    public bool fitX;
+    public bool fitY;
+
+    public RectTransform scrollContent;
+    
+
+    public override void CalculateLayoutInputHorizontal()
+    {
+        base.CalculateLayoutInputHorizontal();
+
+        if (fitType == FitType.Width || fitType == FitType.Height || fitType == FitType.Uniform)
+        {
+
+            fitX = true;
+            fitY = true;
+
+            float sqrt = Mathf.Sqrt(transform.childCount);
+            rows = Mathf.CeilToInt(sqrt);
+            columns = Mathf.CeilToInt(sqrt);
+        }
+
+
+        if(fitType == FitType.Width || fitType == FitType.FixedColumns)
+        {
+            rows = Mathf.CeilToInt(transform.childCount / (float) columns);
+        }
+
+        if (fitType == FitType.Height || fitType == FitType.FixedRows)
+        {
+            columns = Mathf.CeilToInt(transform.childCount / (float) rows);
+        }
+
+
+        float parentWidth = rectTransform.rect.width;
+        float parentHeight = rectTransform.rect.height;
+
+        float cellWidth = (parentWidth / (float)columns) - ((spacing.x / (float)columns) * 2) - (padding.left / (float) columns) - (padding.right / (float) columns);
+        float cellHeight = (parentHeight / (float)rows) - ((spacing.y / (float)rows) * 2) - (padding.top / (float) rows) - (padding.bottom / (float) rows);
+
+        cellSize.x = fitX ? cellWidth : cellSize.x;
+        cellSize.y = fitY ? cellHeight: cellSize.y;
+
+        int colCount = 0;
+        int rowCount = 0;
+
+        scrollContent.sizeDelta = new Vector2(scrollContent.sizeDelta.x, (rectChildren.Count * cellSize.y) + (rectChildren.Count * spacing.y));
+
+        for (int i = 0; i < rectChildren.Count; i++)
+        {
+            rowCount = i / columns;
+            colCount = i % columns;
+
+            var item = rectChildren[i];
+
+            var xPos = (cellSize.x * colCount) + (spacing.x * colCount) + padding.left;
+            var yPos = (cellSize.y * rowCount) + (spacing.y * rowCount) + padding.top;
+
+            SetChildAlongAxis(item, 0, xPos, cellSize.x);
+            SetChildAlongAxis(item, 1, yPos, cellSize.y);
+        }
+    }
+
+    public override void CalculateLayoutInputVertical()
+    {
+
+    }
+
+    public override void SetLayoutHorizontal()
+    {
+
+    }
+
+    public override void SetLayoutVertical()
+    {
+        
+    }
+}
